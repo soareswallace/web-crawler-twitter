495d1d6c0a157564bb5bd78e11de2c0b44ab95b2
Wallace Soares <soareswallace@icloud.com>
2018-10-25 20:53:04 -0300

    organization + stream live tweets saved in json

---
+++ ConnectionAlive.py
@@ -0,0 +1,19 @@
+import tweepy
+from tweepy import Stream, OAuthHandler
+from StreamListener import HashTagListener
+
+#this class search for a hashtag and tracks the stream over twitter and save on python.json
+
+consumer_key = "xsfJwYCkMGIrsHyeqUQmjBWDy"
+consumer_secret = "YbbAx3NzxuHZScTPdmhSGA08KtO5C5YIOJSffjAniatx6N7v9z"
+access_token = "51848952-p4tETlaEQNhTNsuK6dYzebqczKsZ2OMFSonpi75kV"
+access_token_secret = "gwtQz8rN78FclDa4moQRcbG5SukUlX7SnIPrS0VzhaYJo"
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+twitter_stream = Stream(auth, HashTagListener())
+twitter_stream.filter(track=['python'])
+
--- examples.py
+++ examples.py
@@ -1,4 +1,5 @@
 import tweepy
+import json
 
 consumer_key = "xsfJwYCkMGIrsHyeqUQmjBWDy"
 consumer_secret = "YbbAx3NzxuHZScTPdmhSGA08KtO5C5YIOJSffjAniatx6N7v9z"
@@ -12,8 +13,26 @@ auth.set_access_token(access_token, access_token_secret)
 # Creating the API object while passing in auth information
 api = tweepy.API(auth)
 
+username = "katyperry"
+
+# Number of tweets to pull
+tweetCount = 20
+
+def process_or_store(tweet):
+   print(json.dumps(tweet))
+
+#home timeline
 public_tweets = api.home_timeline()
 # foreach through all tweets pulled
 for tweet in public_tweets:
    # printing the text stored inside the tweet object
-   print tweet.text
+   print (tweet.text)
+
+#tweets from my timeline in json format
+for status in tweepy.Cursor(api.user_timeline).items(10):
+   print(process_or_store(status._json))
+
+#search for a especific user
+results = api.user_timeline(id = username, count = tweetCount)
+for tweet in results:
+    print (tweet.text)
